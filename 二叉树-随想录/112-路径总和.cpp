86bb88819cdfb69cbdbde3443e75fd6591ccb8c1
雷昌政 <3375599273@qq.com>
2023-04-18 22:09:57 +0800

    404-左叶子之和（采用递归方法，理解困难）

--- 二叉树-随想录/112-路径总和.cpp
+++ 二叉树-随想录/112-路径总和.cpp
@@ -1,170 +1,170 @@
-using namespace std;
-#pragma region "头文件"
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include<stack>
-#include<queue>
-#include <vector>
-#include <set>
-#include<unordered_set>
-#include<unordered_map>
-#pragma endregin
-
-struct TreeNode
-{
-	int val;
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode() : val(0), left(nullptr), right(nullptr) {}
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-};
-//迭代法
-/*
-void traversal(TreeNode* cur, vector<int>& path, vector<vector<int>>& result)
-{
-	path.push_back(cur->val);
-	if (cur->left == nullptr && cur->right == nullptr)
-	{
-		result.push_back(path);
-		return;
-	}
-	if (cur->left)
-	{
-		traversal(cur->left, path, result);
-		path.pop_back();//我学到了vector里面的pop_back函数,这个叫回退
-	}
-	if (cur->right)
-	{
-		traversal(cur->right, path, result);
-		path.pop_back(); 
-	}
-}
-int sum(vector<int> path)
-{
-	int a = 0;
-	for (auto& i : path)
-	{
-		a += i;
-	}
-	return a;
-}
-bool hasPathSum(TreeNode* root, int targetSum) 
-{
-	vector<vector<int>> result;
-	vector<int> path;
-	if (root == nullptr)
-	{
-		return false;
-	}
-	traversal(root, path, result);
-
-
-	for (auto& i : result)
-	{
-		if (targetSum==sum(i))
-		{
-			return true;
-		}
-	}
-
-	return false;
-}
-*/
-//递归法
-bool findPath(TreeNode* root, int count)
-{
-	if (root==nullptr)
-	{
-		return false;
-	}
-	if (root->left==nullptr && root->right==nullptr && count==0)
-	{
-		return true;
-	}
-	if (root->left == nullptr && root->right == nullptr && count != 0)
-	{
-		return false;
-	}
-
-	if (root->left!=nullptr)
-	{
-		count -= root->left->val;
-		cout << "左 --- 向下" << count << endl;
-		if (findPath(root->left, count))
-		{
-			return true;
-		}
-		//这就是回溯
-		count += root->left->val;
-		cout << "左 --- 向上" << count << endl;
-	}
-	if (root->right != nullptr)
-	{
-		count -= root->right->val;
-		cout << "右 --- 向下" << count << endl;
-		if (findPath(root->right, count))
-		{
-			return true;
-		}
-		count += root->right->val;
-		cout << "右 --- 向上" << count << endl;
-	}
-	return false;
-}
-bool hasPathSum(TreeNode* root, int targetSum)
-{
-
-	if (root == NULL) 
-		return false;
-
-
-	return findPath(root, targetSum - root->val);
-}
-
-
-int main()
-{
-	// 创建一棵二叉树
-	TreeNode* root = new TreeNode(5);
-	TreeNode* node1 = new TreeNode(4);
-	TreeNode* node2 = new TreeNode(8);
-	TreeNode* node3 = new TreeNode(11);
-	TreeNode* node4 = new TreeNode(13);
-	TreeNode* node5 = new TreeNode(4);
-	TreeNode* node6 = new TreeNode(7);
-	TreeNode* node7 = new TreeNode(3);
-	TreeNode* node8 = new TreeNode(1);
-
-	root->left = node1;
-	root->right = node2;
-	node1->left = node3;
-	node2->left = node4;
-	node2->right = node5;
-	node3->left = node6;
-	node3->right = node7;
-	node5->right = node8;
-
-	// 测试路径和是否等于22
-	bool hasPath = hasPathSum(root, 22);
-	if (hasPath) {
-		cout << "存在路径使得路径和等于22" << endl;
-	}
-	else {
-		cout << "不存在路径使得路径和等于22" << endl;
-	}
-
-	// 释放内存
-	delete root;
-	delete node1;
-	delete node2;
-	delete node3;
-	delete node4;
-	delete node5;
-	delete node6;
-	delete node7;
-	delete node8;
-
-	return 0;
-}
+//using namespace std;
+//#pragma region "头文件"
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//#include<stack>
+//#include<queue>
+//#include <vector>
+//#include <set>
+//#include<unordered_set>
+//#include<unordered_map>
+//#pragma endregin
+//
+//struct TreeNode
+//{
+//	int val;
+//	TreeNode* left;
+//	TreeNode* right;
+//	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+//};
+////迭代法
+///*
+//void traversal(TreeNode* cur, vector<int>& path, vector<vector<int>>& result)
+//{
+//	path.push_back(cur->val);
+//	if (cur->left == nullptr && cur->right == nullptr)
+//	{
+//		result.push_back(path);
+//		return;
+//	}
+//	if (cur->left)
+//	{
+//		traversal(cur->left, path, result);
+//		path.pop_back();//我学到了vector里面的pop_back函数,这个叫回退
+//	}
+//	if (cur->right)
+//	{
+//		traversal(cur->right, path, result);
+//		path.pop_back(); 
+//	}
+//}
+//int sum(vector<int> path)
+//{
+//	int a = 0;
+//	for (auto& i : path)
+//	{
+//		a += i;
+//	}
+//	return a;
+//}
+//bool hasPathSum(TreeNode* root, int targetSum) 
+//{
+//	vector<vector<int>> result;
+//	vector<int> path;
+//	if (root == nullptr)
+//	{
+//		return false;
+//	}
+//	traversal(root, path, result);
+//
+//
+//	for (auto& i : result)
+//	{
+//		if (targetSum==sum(i))
+//		{
+//			return true;
+//		}
+//	}
+//
+//	return false;
+//}
+//*/
+////递归法
+//bool findPath(TreeNode* root, int count)
+//{
+//	if (root==nullptr)
+//	{
+//		return false;
+//	}
+//	if (root->left==nullptr && root->right==nullptr && count==0)
+//	{
+//		return true;
+//	}
+//	if (root->left == nullptr && root->right == nullptr && count != 0)
+//	{
+//		return false;
+//	}
+//
+//	if (root->left!=nullptr)
+//	{
+//		count -= root->left->val;
+//		cout << "左 --- 向下" << count << endl;
+//		if (findPath(root->left, count))
+//		{
+//			return true;
+//		}
+//		//这就是回溯
+//		count += root->left->val;
+//		cout << "左 --- 向上" << count << endl;
+//	}
+//	if (root->right != nullptr)
+//	{
+//		count -= root->right->val;
+//		cout << "右 --- 向下" << count << endl;
+//		if (findPath(root->right, count))
+//		{
+//			return true;
+//		}
+//		count += root->right->val;
+//		cout << "右 --- 向上" << count << endl;
+//	}
+//	return false;
+//}
+//bool hasPathSum(TreeNode* root, int targetSum)
+//{
+//
+//	if (root == NULL) 
+//		return false;
+//
+//
+//	return findPath(root, targetSum - root->val);
+//}
+//
+//
+//int main()
+//{
+//	// 创建一棵二叉树
+//	TreeNode* root = new TreeNode(5);
+//	TreeNode* node1 = new TreeNode(4);
+//	TreeNode* node2 = new TreeNode(8);
+//	TreeNode* node3 = new TreeNode(11);
+//	TreeNode* node4 = new TreeNode(13);
+//	TreeNode* node5 = new TreeNode(4);
+//	TreeNode* node6 = new TreeNode(7);
+//	TreeNode* node7 = new TreeNode(3);
+//	TreeNode* node8 = new TreeNode(1);
+//
+//	root->left = node1;
+//	root->right = node2;
+//	node1->left = node3;
+//	node2->left = node4;
+//	node2->right = node5;
+//	node3->left = node6;
+//	node3->right = node7;
+//	node5->right = node8;
+//
+//	// 测试路径和是否等于22
+//	bool hasPath = hasPathSum(root, 22);
+//	if (hasPath) {
+//		cout << "存在路径使得路径和等于22" << endl;
+//	}
+//	else {
+//		cout << "不存在路径使得路径和等于22" << endl;
+//	}
+//
+//	// 释放内存
+//	delete root;
+//	delete node1;
+//	delete node2;
+//	delete node3;
+//	delete node4;
+//	delete node5;
+//	delete node6;
+//	delete node7;
+//	delete node8;
+//
+//	return 0;
+//}
--- 二叉树-随想录/404-左叶子之和.cpp
+++ 二叉树-随想录/404-左叶子之和.cpp
@@ -1,65 +1,119 @@
-//using namespace std;
-//#pragma region "头文件"
-//#include <iostream>
-//#include <string>
-//#include <algorithm>
-//#include<stack>
-//#include<queue>
-//#include <vector>
-//#include <set>
-//#include<unordered_set>
-//#include<unordered_map>
-//#pragma endregin
-//
-//struct TreeNode
-//{
-//	int val;
-//	TreeNode* left;
-//	TreeNode* right;
-//	TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-//};
-////迭代法（我真他马的吊，一遍过）
-//int sumOfLeftLeaves(TreeNode* root) 
-//{
-//	queue<TreeNode*> que;
-//	int sum = 0;
-//	if (root==nullptr)
-//	{
-//		return sum;
-//	}
-//	que.push(root);
-//	int size = que.size();
-//
-//	while (!que.empty())
-//	{
-//		size = que.size();
-//		while (size--)
-//		{
-//			TreeNode* temp = que.front();
-//			que.pop();
-//			if (temp->left!=nullptr)
-//			{
-//				que.push(temp->left);
-//				if (temp->left->left==nullptr && temp->left->right==nullptr)
-//				{
-//					sum += temp->left->val;
-//				}
-//			}
-//			if (temp->right!=nullptr)
-//			{
-//				que.push(temp->right);
-//			}
-//		}
-//	}
-//	return sum;
-//}
-//
-//int main()
-//{
-//
-//
-//
-//	return 0;
-//}
+using namespace std;
+#pragma region "头文件"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include<stack>
+#include<queue>
+#include <vector>
+#include <set>
+#include<unordered_set>
+#include<unordered_map>
+#pragma endregin
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+//迭代法（我真他马的吊，一遍过）
+int sumOfLeftLeaves(TreeNode* root) 
+{
+	queue<TreeNode*> que;
+	int sum = 0;
+	if (root==nullptr)
+	{
+		return sum;
+	}
+	que.push(root);
+	int size = que.size();
+
+	while (!que.empty())
+	{
+		size = que.size();
+		while (size--)
+		{
+			TreeNode* temp = que.front();
+			que.pop();
+			if (temp->left!=nullptr)
+			{
+				que.push(temp->left);
+				if (temp->left->left==nullptr && temp->left->right==nullptr)
+				{
+					sum += temp->left->val;
+				}
+			}
+			if (temp->right!=nullptr)
+			{
+				que.push(temp->right);
+			}
+		}
+	}
+	return sum;
+}
+//递归法
+/*
+	递归参数：节点       返回值：该分支的左节点
+	终止条件：（该节点的左节点的左节点==空  &&  该节点的左节点的右结点==空）|| 节点==空
+	单层递归逻辑：在代码中，先判断当前结点有没有左右子结点。如果有左孩子，而且这个左孩子的左右孩子是空的，则加上这个结点的
+	数值。
+
+	
+*/
+int sumOfLeftLeaves1(TreeNode* root)
+{
+	if (root==nullptr)
+	{
+		return 0;
+	}
+	if (root->left==nullptr && root->right==nullptr)
+	{
+		return 0;
+	}
+
+	int left = sumOfLeftLeaves1(root->left);
+	if (root->left!=nullptr && root->left->left==nullptr && root->left->right==nullptr)
+	{
+		left = root->left->val;
+	}
+	int right = sumOfLeftLeaves1(root->right);
+
+	int sum = left + right;
+	return sum;
+
+}
+int main()
+{
+	// 创建一棵二叉树
+	TreeNode* root = new TreeNode(3);
+	TreeNode* node1 = new TreeNode(9);
+	TreeNode* node2 = new TreeNode(20);
+	TreeNode* node3 = new TreeNode(15);
+	TreeNode* node4 = new TreeNode(7);
+
+	root->left = node1;
+	root->right = node2;
+	node2->left = node3;
+	node2->right = node4;
+
+	// 计算左叶子之和
+
+	int sum = sumOfLeftLeaves1(root);
+	cout << "左叶子之和为：" << sum << endl;
+
+	// 释放内存
+	delete root;
+	delete node1;
+	delete node2;
+	delete node3;
+	delete node4;
+
+	return 0;
+
+
+
+}
