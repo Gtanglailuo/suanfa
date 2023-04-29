670e4887e93524f2a78a870fb68524b0868c9185
雷昌政 <3375599273@qq.com>
2023-04-29 17:09:46 +0800

    力扣王

--- 二叉树-随想录/108-将有序数组转换成二叉搜索树.cpp
+++ 二叉树-随想录/108-将有序数组转换成二叉搜索树.cpp
@@ -1,50 +1,51 @@
-using namespace std;
-#pragma region "头文件"
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include<stack>
-#include<queue>
-#include <vector>
-#include <set>
-#include<unordered_set>
-#include<unordered_map>
-#pragma endregin
-
-struct TreeNode
-{
-	int val;
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode() : val(0), left(nullptr), right(nullptr) {}
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-};
-
-TreeNode* func(vector<int>& nums,int left,int right)
-{
-	if (left>right)
-	{
-		return nullptr;
-	}
-	int mid = left + (right - left) / 2;
-
-	TreeNode* root = new TreeNode(nums[mid]);
-	root->left = func(nums,left,mid-1);
-	root->right = func(nums,mid+1,right);
-
-	return root;
-}
-TreeNode* sortedArrayToBST(vector<int>& nums) 
-{
-
-	TreeNode* root = func(nums,0,nums.size()-1);
-	return root;
-}
-int main()
-{
-
-
-
-	return 0;
-}
+//using namespace std;
+//#pragma region "头文件"
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//#include<stack>
+//#include<queue>
+//#include <vector>
+//#include <set>
+//#include<unordered_set>
+//#include<unordered_map>
+//#pragma endregin
+//
+//struct TreeNode
+//{
+//	int val;
+//	TreeNode* left;
+//	TreeNode* right;
+//	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+//};
+//
+//TreeNode* func(vector<int>& nums,int left,int right)
+//{
+//	if (left>right)
+//	{
+//		return nullptr;
+//	}
+//	int mid = left + (right - left) / 2;
+//
+//	TreeNode* root = new TreeNode(nums[mid]);
+//	//我这个节点的左边又是一个新的节点，重构
+//	root->left = func(nums,left,mid-1);
+//	root->right = func(nums,mid+1,right);
+//
+//	return root;
+//}
+//TreeNode* sortedArrayToBST(vector<int>& nums) 
+//{
+//
+//	TreeNode* root = func(nums,0,nums.size()-1);
+//	return root;
+//}
+//int main()
+//{
+//
+//
+//
+//	return 0;
+//}
